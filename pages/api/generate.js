5c26c28c3e460bfe20a53cb87a1a189a4644fafa
Macbook <macbook@asuna.local>
2022-12-01 14:12:08 +0530

    first commit

---
+++ pages/api/generate.js
@@ -0,0 +1,33 @@
+// added 
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// addded more
+const basePromptPrefix =
+`
+give me ideas for a gtp-3 prompt base project based on this 
+
+topic:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 400,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
